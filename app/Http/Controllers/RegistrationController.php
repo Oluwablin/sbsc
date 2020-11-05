d31e2831d51ba1dcbaec3dfeaac7ef566699bd13
oluwablin <oyelekeseun@outlook.com>
2020-11-05 19:58:15 +0100

    registers user

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -10,5 +10,47 @@ use App\User;
 
 class RegistrationController extends Controller
 {
-    //
+    /*
+    |-----------------------------------------
+    | REGISTER A USER
+    |-----------------------------------------
+    */
+    public function registerUser(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            $validator                  = Validator::make($request->all(), [
+	        'first_name'                => 'required|string|max:255',
+	        'last_name'                 => 'required|string|max:255',
+	        'email'                     => 'required|string|email|max:255|unique:users',
+	        'password'                  => 'required|string|min:4|confirmed',
+            ]);
+            
+		    if ($validator->fails()) {
+		        return response()->json($validator->errors(), 400);
+            }
+            $user                       = new User;
+		    $user->first_name           = $request->first_name;
+		    $user->last_name            = $request->last_name;
+		    $user->email                = $request->email;
+		    $user->password             = bcrypt($request->password);
+		    $user->code                 = uniqid();
+		    if($user->save()){
+                $data = [
+                    "status"            => "success",
+                    "message"           => 'User account has been created successfully.',
+                ];
+            }else{
+                $data = [
+                    "status"            => "Failed",
+                    "message"           => "Error Encountered while trying to create new account",
+                ];
+            }
+            DB::commit();
+            return response()->json($data, 201);
+        } catch (Exception $e) {
+            DB::rollback();
+            return response()->json(['status'=> 'error' , 'message' => $e->getMessage(). ',' . 'Sorry, could not create account'], 400);
+        }
+    }
 }
--- app/User.php
+++ app/User.php
@@ -6,13 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
-use Zizaco\Entrust\Traits\EntrustUserTrait;
 
 
 class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
-    use EntrustUserTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -42,4 +40,24 @@ class User extends Authenticatable implements JWTSubject
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
