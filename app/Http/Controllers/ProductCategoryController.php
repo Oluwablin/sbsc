9d6d58f1291d8c54c67e530b937b4e0ccef4f448
oluwablin <oyelekeseun@outlook.com>
2020-11-06 01:19:22 +0100

    CRUD for Product Category Created

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -18,6 +18,9 @@ class ProductCategoryController extends Controller
     */
     public function listAllProductCategories(Request $request)
     {
+        $productCategory                   = ProductCategory::findorFail($id);
+
+        return response()->json($productCategory, 200);
     }
 
     /*
@@ -27,6 +30,9 @@ class ProductCategoryController extends Controller
     */
     public function listAProductCategory(Request $request, $id)
     {
+        $productCategory                   = ProductCategory::paginate(10);
+
+        return response()->json($productCategory, 200);
     }
 
     /*
@@ -36,6 +42,20 @@ class ProductCategoryController extends Controller
     */
     public function storeProductCategory(Request $request)
     {
+        $user_id                            = Auth::id();
+
+        try {
+            DB::beginTransaction();
+            $productCategory                = new ProductCategory($request->all());
+            $productCategory->created_by    = $user_id;
+            $productCategory->updated_by    = $user_id;
+            $productCategory->save();
+            DB::commit();
+            return response()->json(['status' => 'success', 'data' => $productCategory, 'message' => 'Product Category Created Successfully'], 201);
+        } catch (Exception $e) {
+            DB::rollback();
+            return response()->json(['status' => 'error', 'message' => $e->getMessage(). ',' . ' Product Category could not be created, Please ensure data is inputted correctly.'], 400);
+        }
     }
 
     /*
@@ -45,6 +65,19 @@ class ProductCategoryController extends Controller
     */
     public function updateProductCategory(Request $request, $id)
     {
+        $user_id                            = Auth::id();
+
+        try {
+            DB::beginTransaction();
+            $productCategory                = ProductCategory::findorFail($id);
+            $productCategory->updated_by    = $user_id;
+            $productCategory->update($request->except(['_token']));
+            DB::commit();
+            return response()->json(['status' => 'success', 'data' => $productCategory, 'message' => 'Product Category Updated Successfully'], 201);
+        } catch (Exception $e) {
+            DB::rollback();
+            return response()->json(['status' => 'error', 'message' => $e->getMessage(). ',' . ' Product Category could not be updated, Please ensure data is inputted correctly.'], 400);
+        }
     }
 
     /*
@@ -54,5 +87,26 @@ class ProductCategoryController extends Controller
     */
     public function deleteProductCategory(Request $request, $id)
     {
+        $productCategory                       = ProductCategory::find($id);
+        if($productCategory !== null){
+            if($productCategory->delete()){
+                $data = [
+                    "status"        => "success",
+                    "message"       => "Product Category was deleted successfully"
+                ];
+            }else{
+                $data = [
+                    "status"        => "error",
+                    "message"       => "Error deleting Product Category!"
+                ];
+            }
+        }else{
+            $data = [
+                "status"            => "error",
+                "message"           => "No Product Category found!"
+            ];
+        }
+
+        return response()->json($data, 200);
     }
 }
